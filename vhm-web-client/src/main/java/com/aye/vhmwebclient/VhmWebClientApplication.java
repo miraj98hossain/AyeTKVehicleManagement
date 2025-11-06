c3693ea55143f268fc03df058a806b2be83de747
Miraj <miraj09hossain@gmai.com>
2025-11-06 10:55:51 +0600

    Web Client mod to lib

--- vhm-web-client/src/main/java/com/aye/vhmwebclient/VhmWebClientApplication.java
+++ vhm-web-client/src/main/java/com/aye/vhmwebclient/VhmWebClientApplication.java
@@ -3,13 +3,13 @@ package com.aye.vhmwebclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+//import org.springframework.data.web.config.EnableSpringDataWebSupport;
+//
+//import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+//@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class VhmWebClientApplication {
 
     public static void main(String[] args) {
