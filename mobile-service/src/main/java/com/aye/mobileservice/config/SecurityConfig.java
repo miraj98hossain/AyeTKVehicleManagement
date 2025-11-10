fb9a2ffb63093cca8162a65501f09c59ba6de650
Miraj <miraj09hossain@gmai.com>
2025-11-10 10:26:23 +0600

    Update: for doing basic auth CustomAuthenticationProvider made inactive. created CustomAuthFilter added in the filter chain in security.

---
+++ mobile-service/src/main/java/com/aye/mobileservice/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.aye.mobileservice.config;
+
+import com.aye.mobileservice.securityfilter.CustomAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomAuthFilter customAuthFilter;
+
+    public SecurityConfig(CustomAuthFilter customAuthFilter) {
+        this.customAuthFilter = customAuthFilter;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(customAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
+
