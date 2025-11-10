e556290513d628a9d13f5b5d0b6e6afde173ffc2
Miraj <miraj09hossain@gmai.com>
2025-11-10 10:21:38 +0600

    Update: ssl configuration changed. downgraded to http4 version. due to spring 2.7.3 version.

--- mobile-service/src/main/java/com/aye/mobileservice/config/RestTemplateConfig.java
+++ mobile-service/src/main/java/com/aye/mobileservice/config/RestTemplateConfig.java
@@ -1,13 +1,9 @@
 package com.aye.mobileservice.config;
 
-
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
-import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -17,49 +13,34 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Value("${server.ssl.trust-store}")
     private Resource trustStore;
-
     @Value("${server.ssl.trust-store-password}")
     private String trustStorePassword;
     @Value("${server.ssl.key-store}")
     private Resource keyStore;
-
     @Value("${server.ssl.key-store-password}")
     private String keyStorePassword;
 
     @Bean
-    public CloseableHttpClient customHttpClient() throws IOException, GeneralSecurityException {
-
+    public RestTemplate restTemplateWithTrustStore(RestTemplateBuilder builder) throws Exception {
         SSLContext sslContext = SSLContexts.custom()
                 .loadKeyMaterial(keyStore.getURL(), keyStorePassword.toCharArray(), keyStorePassword.toCharArray())
                 .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
                 .build();
 
-        final SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactoryBuilder.create()
-                .setSslContext(sslContext)
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(socketFactory)   // ✅ now works
                 .build();
 
-        final HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(sslSocketFactory)
-                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
-        return HttpClients.custom()
-                .setConnectionManager(cm)
-                .evictExpiredConnections().build();
-    }
-
-    @Bean
-    public RestTemplate restTemplateWithTrustStore(RestTemplateBuilder builder, CloseableHttpClient customHttpClient) {
-        return builder
-                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(customHttpClient))
-                .build();
+        return builder.requestFactory(() -> requestFactory).build();
     }
 }
-
