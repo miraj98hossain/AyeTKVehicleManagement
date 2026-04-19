e919b684a9b0df9c57d07c12624fe9426cc4268d
miraj98hossain <miraj09hossain@gmail.com>
2026-04-19 14:08:34 +0600

    Lombok annotation added in UndelvOrderListDto

--- aye-dto-lib/src/main/java/com/aye/dtoLib/dto/response/schedule/UndelvOrderListDto.java
+++ aye-dto-lib/src/main/java/com/aye/dtoLib/dto/response/schedule/UndelvOrderListDto.java
@@ -1,5 +1,12 @@
 package com.aye.dtoLib.dto.response.schedule;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UndelvOrderListDto {
     Long orgId;
 
