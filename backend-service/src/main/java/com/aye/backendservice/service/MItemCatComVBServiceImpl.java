fe786bc16ba7dfafa12d41c52fb8f7fe1c748e8b
miraj98hossain <miraj09hossain@gmail.com>
2026-04-29 17:55:14 +0600

    minor indentation modified

--- backend-service/src/main/java/com/aye/backendservice/service/MItemCatComVBServiceImpl.java
+++ backend-service/src/main/java/com/aye/backendservice/service/MItemCatComVBServiceImpl.java
@@ -1,14 +1,16 @@
 package com.aye.backendservice.service;
 
 import com.aye.RestfulServer.service.MItemCatComVService;
-
 import com.aye.dtoLib.dto.response.ApiRequestResponse;
 import com.aye.dtoLib.dto.response.ApiRequestResponseDetail;
 import com.aye.dtoLib.dto.response.MItemCatComVResponse;
+import com.aye.entitylib.entity.MItemCatComV;
 import com.aye.mapper.order.MItemCatComVMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MItemCatComVBServiceImpl implements MItemCatComVBService {
@@ -31,14 +33,14 @@ public class MItemCatComVBServiceImpl implements MItemCatComVBService {
 
     @Override
     public ApiRequestResponse filterItemCatComb(Long orgId, String searchWords) {
+        List<MItemCatComV> list = mItemCatComVService.filterItemCatComb(orgId, searchWords);
         return ApiRequestResponseMaker.make(
                 HttpStatus.OK.name(),
                 "Success",
                 ApiRequestResponseDetail.ObjectType.A,
                 "itemCatComb",
                 MItemCatComVResponse.class.getName(),
-                mItemCatComVService.filterItemCatComb(orgId, searchWords)
-                        .stream()
+                list.stream()
                         .map(mItemCatComVMapper::toResponseDto).toList()
         );
     }
